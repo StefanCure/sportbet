8aee6070ea3a05a818e1db834cac7f37c9d4f679
stefancure <akpanowoprecious@gmail.com>
2021-07-01 14:54:23 +0100

    started including important stuff

--- 17-stripe-submenus/final/app.js
+++ 17-stripe-submenus/final/app.js
@@ -21,60 +21,9 @@ sidebar.innerHTML = sublinks
   .map((item) => {
     const { links, page } = item;
     return `<article >
-<h4>${page}</h4>
-<div class="sidebar-sublinks">
-${links
-  .map((link) => {
-    return `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`;
-  })
-  .join('')}
-</div>
+<h4><a href="${links}" class"link_color">${page}</a></h4>
 </article>`;
   })
   .join('');
 
-linkBtns.forEach((btn) => {
-  btn.addEventListener('mouseover', function (e) {
-    const text = e.currentTarget.textContent;
-    const tempBtn = e.currentTarget.getBoundingClientRect();
-    const center = (tempBtn.left + tempBtn.right) / 2;
-    const bottom = tempBtn.bottom - 3;
 
-    const tempPage = sublinks.find((link) => link.page === text);
-    if (tempPage) {
-      const { page, links } = tempPage;
-      submenu.classList.add('show');
-      submenu.style.left = `${center}px`;
-      submenu.style.top = `${bottom}px`;
-      // OPTIONAL
-      let columns = 'col-2';
-      if (links.length === 3) {
-        columns = 'col-3';
-      }
-      if (links.length > 3) {
-        columns = 'col-4';
-      }
-      submenu.innerHTML = `
-      <section> 
-      <h4>${page}</h4>
-      <div class="submenu-center ${columns}">
-      ${links
-        .map((link) => {
-          return `<a href="${link.url}"><i class="${link.icon}"></i>${link.label}</a>`;
-        })
-        .join('')}
-      </div>
-      </section>
-      `;
-    }
-  });
-});
-
-hero.addEventListener('mouseover', function (e) {
-  submenu.classList.remove('show');
-});
-nav.addEventListener('mouseover', function (e) {
-  if (!e.target.classList.contains('link-btn')) {
-    submenu.classList.remove('show');
-  }
-});
--- 17-stripe-submenus/final/data.js
+++ 17-stripe-submenus/final/data.js
@@ -1,27 +1,23 @@
 const sublinks = [
   {
-    page: 'products',
-    links: [
-      { label: 'payment', icon: 'fas fa-credit-card', url: 'products.html' },
-      { label: 'terminal', icon: 'fas fa-credit-card', url: 'products.html' },
-      { label: 'connect', icon: 'fas fa-credit-card', url: 'products.html' },
-    ],
+    page: 'HOME',
+    links:  'index.html'
   },
   {
-    page: 'developers',
-    links: [
-      { label: 'plugins', icon: 'fas fa-book', url: 'products.html' },
-      { label: 'libraries', icon: 'fas fa-book', url: 'products.html' },
-      { label: 'plugins', icon: 'fas fa-book', url: 'products.html' },
-      { label: 'billing', icon: 'fas fa-book', url: 'products.html' },
-    ],
+    page: 'FOOTBALL',
+    links:  'products.html'
   },
   {
-    page: 'company',
-    links: [
-      { label: 'about', icon: 'fas fa-briefcase', url: 'products.html' },
-      { label: 'customers', icon: 'fas fa-briefcase', url: 'products.html' },
-    ],
+    page: 'BASKETBALL',
+    links: 'products.html'
+  },
+  {
+    page: 'HANDBALL',
+    links: 'products.html'
+  },
+  {
+    page: 'LIVE SCORE',
+    links: 'products.html'
   },
 ];
 
