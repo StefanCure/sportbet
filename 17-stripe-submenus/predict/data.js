00c56fda4120f7386b790a330878a954dc4431e0
stefancure <akpanowoprecious@gmail.com>
2021-07-03 20:06:59 +0100

    my__header

---
+++ 17-stripe-submenus/predict/data.js
@@ -0,0 +1,20 @@
+const sublinks = [
+  {
+    page: 'HOME',
+    links:  'index.html'
+  },
+  {
+    page: 'TIPSCORE',
+    links:  'products.html'
+  },
+  {
+    page: 'ABOUT',
+    links: 'products.html'
+  },
+  {
+    page: 'CONTACT US',
+    links: 'products.html'
+  },
+];
+
+export default sublinks;
---
+++ 17-stripe-submenus/predict1/app.js
@@ -0,0 +1,29 @@
+import sublinks from './data.js';
+
+const toggleBtn = document.querySelector('.toggle-btn');
+const closeBtn = document.querySelector('.close-btn');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+const sidebar = document.querySelector('.sidebar-links');
+const linkBtns = [...document.querySelectorAll('.link-btn')];
+const submenu = document.querySelector('.submenu');
+const hero = document.querySelector('.hero');
+const nav = document.querySelector('.nav');
+// hide/show sideabar
+toggleBtn.addEventListener('click', () => {
+  sidebarWrapper.classList.add('show');
+});
+closeBtn.addEventListener('click', () => {
+  sidebarWrapper.classList.remove('show');
+});
+
+// set sidebar
+sidebar.innerHTML = sublinks
+  .map((item) => {
+    const { links, page } = item;
+    return `<article >
+<h4><a href="${links}" class"link_color">${page}</a></h4>
+</article>`;
+  })
+  .join('');
+
+
