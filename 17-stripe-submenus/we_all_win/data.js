33b3bc153080217d94b8a15a9547d7fba8e890b3
stefancure <akpanowoprecious@gmail.com>
2021-07-10 12:30:30 +0100

    my_first_complete_bet_site

---
+++ 17-stripe-submenus/we_all_win/data.js
@@ -0,0 +1,20 @@
+const sublinks = [
+  {
+    page: 'HOME',
+    links:  'index.html'
+  },
+  {
+    page: 'TIPSCORE',
+    links:  'index.html'
+  },
+  {
+    page: 'ABOUT',
+    links: 'about.html'
+  },
+  {
+    page: 'CONTACT US',
+    links: 'contact.html'
+  },
+];
+
+export default sublinks;
